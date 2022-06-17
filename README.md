fb4005f91c6ac740697d2f4e0b23e9a38336f945
James-koduah <jameskoduah171@gmail.com>
2022-06-17 17:30:58 +0100

    Improved website
    Added the changing pictures to the project section
    Added the react and JavaScript logo to my Home page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Personal-Portfolio
 My personal webpage containing my projects on github
+It has currently been built with mainly html and CSS with a hint of JavaScript functionality
--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+let prokey=document.getElementById('keyboard')
+let proquo=document.getElementById('quotes')
+let jj=2;
 function copy (){
    navigator.clipboard.writeText('jameskoduah171@gmail.com');
   alert("Copied the text: jameskoduah171@gmail.com");
 }
+
+setInterval(()=>{
+  if(jj==1){
+    prokey.style.background='url(images/keyboard.JPG)';
+    prokey.style.backgroundSize='100% 100%'
+    proquo.style.background='url(images/quotes.JPG)';
+    proquo.style.backgroundSize='100% 100%';
+    jj=2;
+  }else if(jj===2){
+    prokey.style.background='url(images/keyboard2.JPG)';
+    proquo.style.background='url(images/quotes2.JPG)';
+    proquo.style.backgroundSize='100% 100%';
+    prokey.style.backgroundSize='100% 100%';
+    jj=3;
+  }else if(jj==3){
+    prokey.style.background='url(images/keyboard3.JPG)';
+    prokey.style.backgroundSize='100% 100%'
+    proquo.style.background='url(images/quotes3.JPG)';
+    proquo.style.backgroundSize='100% 100%';
+    jj=1;
+  }
+},4000)
